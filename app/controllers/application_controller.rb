bb5fd29a1231de2cc847597f589a7c02f1f50a4e
Oluebube Princess Egbuna <egbunaoluebube@gmail.com>
2020-11-24 13:12:40 +0100

    Complete Singup and signin forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_controller, if: :devise_controller?
+
+  protected
+
+  def configure_devise_controller
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :profile_url])
+
+  end
 end
